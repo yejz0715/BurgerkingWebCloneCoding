03390186d784c86067bf43c6668525ba46391caa
YataNox <rladnwls0523@naver.com>
2021-11-19 20:05:39 +0900

    Code Optimization

--- WEB_Burgerking/src/main/java/com/burger/controller/BurgerKingServlet.java
+++ WEB_Burgerking/src/main/java/com/burger/controller/BurgerKingServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.burger.controller.*;
 import com.burger.controller.action.Action;
 
 /**
