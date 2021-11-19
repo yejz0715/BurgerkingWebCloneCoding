028509eb1ea1591a8fabd483683782273b8160e2
YataNox <rladnwls0523@naver.com>
2021-11-19 22:37:15 +0900

    Add command else if case - terms, privacy, legal

--- WEB_Burgerking/src/main/java/com/burger/controller/ActionFactory.java
+++ WEB_Burgerking/src/main/java/com/burger/controller/ActionFactory.java
@@ -2,6 +2,9 @@ package com.burger.controller;
 
 import com.burger.controller.action.Action;
 import com.burger.controller.action.IndexAction;
+import com.burger.controller.action.LegalAction;
+import com.burger.controller.action.PrivacyAction;
+import com.burger.controller.action.TermsAction;
 
 public class ActionFactory {
 	private ActionFactory() {}
@@ -12,6 +15,9 @@ public class ActionFactory {
 		Action ac = null;
 		
 		if (command.equals("index")) ac = new IndexAction();
+		else if(command.equals("terms")) ac = new TermsAction();
+		else if(command.equals("privacy")) ac = new PrivacyAction();
+		else if(command.equals("legal")) ac = new LegalAction();
 		
 		return ac;
 	}
