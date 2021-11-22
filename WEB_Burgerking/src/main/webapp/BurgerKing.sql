43a83fc344a80b45441c4d6d9cbee98cf1500319
yejz0715 <yejz0715@gmail.com>
2021-11-23 02:15:05 +0900

    event_img,eventdetail_img,eventinsert,eventlist.jsp

--- WEB_Burgerking/src/main/webapp/BurgerKing.sql
+++ WEB_Burgerking/src/main/webapp/BurgerKing.sql
@@ -32,30 +32,27 @@ DROP TABLE sub_product CASCADE CONSTRAINTS;
 
 /* Drop Sequences */
 
-DROP SEQUENCE SEQ_cart_cseq;
-DROP SEQUENCE SEQ_event_eseq;
-DROP SEQUENCE SEQ_Member_membernum;
-DROP SEQUENCE SEQ_member_mseq;
-DROP SEQUENCE SEQ_order_detail_odseq;
-DROP SEQUENCE SEQ_product_productnum;
-DROP SEQUENCE SEQ_product_pseq;
-DROP SEQUENCE SEQ_qna_qseq;
-DROP SEQUENCE SEQ_sub_product_spseq;
+DROP SEQUENCE cseq;
+DROP SEQUENCE eseq;
+DROP SEQUENCE mseq;
+DROP SEQUENCE oseq;
+DROP SEQUENCE pseq;
+DROP SEQUENCE qseq;
+DROP SEQUENCE spseq;
 
 
 
 
 /* Create Sequences */
 
-CREATE SEQUENCE SEQ_cart_cseq INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_event_eseq INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_Member_membernum INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_member_mseq INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_order_detail_odseq INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_product_productnum INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_product_pseq INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_qna_qseq INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_sub_product_spseq INCREMENT BY 1 START WITH 1;
+create sequence mseq increment by 1 start with 1;
+create sequence qseq increment by 1 start with 1;
+create sequence cseq increment by 1 start with 1;
+create sequence oseq increment by 1 start with 1;
+create sequence pseq increment by 1 start with 1;
+create sequence spseq increment by 1 start with 1;
+create sequence eseq increment by 1 start with 1;
+
 
 
 
@@ -63,146 +60,147 @@ CREATE SEQUENCE SEQ_sub_product_spseq INCREMENT BY 1 START WITH 1;
 
 CREATE TABLE address
 (
-	zip_num varchar2(7) NOT NULL,
-	sido varchar2(30) NOT NULL,
-	gugun varchar2(30) NOT NULL,
-	dong varchar2(100) NOT NULL,
-	zip_code varchar2(30),
-	bunji varchar2(30)
+   zip_num varchar2(7) NOT NULL,
+   sido varchar2(30) NOT NULL,
+   gugun varchar2(30) NOT NULL,
+   dong varchar2(100) NOT NULL,
+   zip_code varchar2(30),
+   bunji varchar2(30)
 );
 
 
 CREATE TABLE admin
 (
-	id varchar2(20) NOT NULL,
-	pwd varchar2(30) NOT NULL,
-	name varchar2(20) NOT NULL,
-	phone varchar2(20) NOT NULL,
-	PRIMARY KEY (id)
+   id varchar2(20) NOT NULL,
+   pwd varchar2(30) NOT NULL,
+   name varchar2(20) NOT NULL,
+   phone varchar2(20) NOT NULL,
+   PRIMARY KEY (id)
 );
 
 
 CREATE TABLE cart
 (
-	cseq number(10) NOT NULL,
-	id varchar2(50) NOT NULL,
-	pseq number(10),
-	quantity number(5) DEFAULT 1,
-	result varchar2(1) DEFAULT '1',
-	indate date DEFAULT sysdate,
-	PRIMARY KEY (cseq)
+   cseq number(10) NOT NULL,
+   id varchar2(50) NOT NULL,
+   pseq number(10),
+   quantity number(5) DEFAULT 1,
+   result varchar2(1) DEFAULT '1',
+   indate date DEFAULT sysdate,
+   PRIMARY KEY (cseq)
 );
 
 
 CREATE TABLE event
 (
-	eseq number(10) NOT NULL,
-	subject varchar2(30) NOT NULL,
-	content varchar2(100) NOT NULL,
-	image varchar2(50),
-	startdate date DEFAULT sysdate,
-	enddate date,
-	state number(1) DEFAULT 1,
-	PRIMARY KEY (eseq)
+   eseq number(10) NOT NULL,
+   subject varchar2(30) NOT NULL,
+   content varchar2(2000) NOT NULL,
+   image varchar2(50),
+   startdate date DEFAULT sysdate,
+   enddate date,
+   state number(1) DEFAULT 1,
+   PRIMARY KEY (eseq)
 );
 
 
 CREATE TABLE member
 (
-	mseq number(10) NOT NULL,
-	id varchar2(50) NOT NULL UNIQUE,
-	pwd varchar2(20) NOT NULL,
-	phone varchar2(13) NOT NULL,
-	indate date DEFAULT sysdate,
-	lastdate date DEFAULT sysdate,
-	name varchar2(5) NOT NULL,
-	memberkind number(1) DEFAULT 1,
-	PRIMARY KEY (mseq)
+   mseq number(10) NOT NULL,
+   id varchar2(50) NOT NULL UNIQUE,
+   pwd varchar2(20) NOT NULL,
+   phone varchar2(13) NOT NULL,
+   indate date DEFAULT sysdate,
+   lastdate date DEFAULT sysdate,
+   name varchar2(5) NOT NULL,
+   memberkind number(1) DEFAULT 1,
+   PRIMARY KEY (mseq)
 );
 
 
 CREATE TABLE Myaddress
 (
-	mseq number(10) NOT NULL,
-	address varchar2(100) NOT NULL,
-	aname varchar2(10) NOT NULL,
-	zip_num varchar2(7),
-	PRIMARY KEY (mseq)
+   mseq number(10) NOT NULL,
+   address varchar2(100) NOT NULL,
+   aname varchar2(10) NOT NULL,
+   zip_num varchar2(7),
+   PRIMARY KEY (mseq)
 );
 
 
 CREATE TABLE non_member
 (
-	id varchar2(50) NOT NULL,
-	password varchar2(20) NOT NULL,
-	phone varchar2(13) NOT NULL,
-	memberkind number(1) DEFAULT 2,
-	PRIMARY KEY (id, password)
+   id varchar2(50) NOT NULL,
+   password varchar2(20) NOT NULL,
+   phone varchar2(13) NOT NULL,
+   memberkind number(1) DEFAULT 2,
+   PRIMARY KEY (id, password)
 );
 
 
 CREATE TABLE orders
 (
-	oseq number(10) NOT NULL,
-	id varchar2(50) NOT NULL,
-	indate  date DEFAULT sysdate,
-	memberkind number(1) NOT NULL,
-	PRIMARY KEY (oseq)
+   oseq number(10) NOT NULL,
+   id varchar2(50) NOT NULL,
+   indate  date DEFAULT sysdate,
+   memberkind number(1) NOT NULL,
+   PRIMARY KEY (oseq)
 );
 
 
 CREATE TABLE order_detail
 (
-	odseq number(10) NOT NULL,
-	oseq number(10),
-	pseq number(10),
-	quantity number(5) DEFAULT 1,
-	result number(1) DEFAULT '1',
-	PRIMARY KEY (odseq)
+   odseq number(10) NOT NULL,
+   oseq number(10),
+   pseq number(10),
+   quantity number(5) DEFAULT 1,
+   result number(1) DEFAULT '1',
+   PRIMARY KEY (odseq)
 );
 
 
 CREATE TABLE product
 (
-	pseq number(10) NOT NULL,
-	pname varchar2(100) NOT NULL,
-	price1 number(10) NOT NULL,
-	price2 number(10) NOT NULL,
-	price3 number(10) NOT NULL,
-	kind1 varchar2(5) NOT NULL,
-	kind2 varchar2(3) NOT NULL,
-	indate  date DEFAULT sysdate NOT NULL,
-	content varchar2(100),
-	image varchar2(50),
-	useyn number(1) DEFAULT 1 NOT NULL,
-	PRIMARY KEY (pseq)
+   pseq number(10) NOT NULL,
+   pname varchar2(100) NOT NULL,
+   price1 number(10) NOT NULL,
+   price2 number(10) NOT NULL,
+   price3 number(10) NOT NULL,
+   kind1 varchar2(5) NOT NULL,
+   kind2 varchar2(3) NOT NULL,
+   kind3 varchar2(3) NOT NULL,
+   indate  date DEFAULT sysdate NOT NULL,
+   content varchar2(100),
+   image varchar2(50),
+   useyn number(1) DEFAULT 1 NOT NULL,
+   PRIMARY KEY (pseq)
 );
 
 
 CREATE TABLE qna
 (
-	qseq number(30) NOT NULL,
-	subject varchar2(30) NOT NULL,
-	content varchar2(50) NOT NULL,
-	indate date DEFAULT sysdate,
-	id varchar2(50) NOT NULL,
-	reply varchar2(30),
-	rep char DEFAULT '1',
-	readcount number(5),
-	pass number(4) NOT NULL,
-	PRIMARY KEY (qseq)
+   qseq number(30) NOT NULL,
+   subject varchar2(30) NOT NULL,
+   content varchar2(50) NOT NULL,
+   indate date DEFAULT sysdate,
+   id varchar2(50) NOT NULL,
+   reply varchar2(30),
+   rep char DEFAULT '1',
+   readcount number(5),
+   pass number(4) NOT NULL,
+   PRIMARY KEY (qseq)
 );
 
 
 CREATE TABLE sub_product
 (
-	spseq number(10) NOT NULL,
-	sname varchar2(10) NOT NULL,
-	kind1 number(5),
-	kind2 varchar2(3) DEFAULT '0-0',
-	addprice number(5) NOT NULL,
-	image varchar2(50),
-	PRIMARY KEY (spseq)
+   spseq number(10) NOT NULL,
+   sname varchar2(10) NOT NULL,
+   kind1 number(5),
+   kind2 varchar2(3) DEFAULT '0-0',
+   addprice number(5) NOT NULL,
+   image varchar2(50),
+   PRIMARY KEY (spseq)
 );
 
 
@@ -210,38 +208,38 @@ CREATE TABLE sub_product
 /* Create Foreign Keys */
 
 ALTER TABLE cart
-	ADD FOREIGN KEY (id)
-	REFERENCES member (id)
+   ADD FOREIGN KEY (id)
+   REFERENCES member (id)
 ;
 
 
 ALTER TABLE Myaddress
-	ADD FOREIGN KEY (mseq)
-	REFERENCES member (mseq)
+   ADD FOREIGN KEY (mseq)
+   REFERENCES member (mseq)
 ;
 
 
 ALTER TABLE qna
-	ADD FOREIGN KEY (id)
-	REFERENCES member (id)
+   ADD FOREIGN KEY (id)
+   REFERENCES member (id)
 ;
 
 
 ALTER TABLE order_detail
-	ADD FOREIGN KEY (oseq)
-	REFERENCES orders (oseq)
+   ADD FOREIGN KEY (oseq)
+   REFERENCES orders (oseq)
 ;
 
 
 ALTER TABLE cart
-	ADD FOREIGN KEY (pseq)
-	REFERENCES product (pseq)
+   ADD FOREIGN KEY (pseq)
+   REFERENCES product (pseq)
 ;
 
 
 ALTER TABLE order_detail
-	ADD FOREIGN KEY (pseq)
-	REFERENCES product (pseq)
+   ADD FOREIGN KEY (pseq)
+   REFERENCES product (pseq)
 ;
 
 
@@ -251,108 +249,99 @@ ALTER TABLE order_detail
 CREATE OR REPLACE TRIGGER TRI_cart_cseq BEFORE INSERT ON cart
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_cart_cseq.nextval
-	INTO :new.cseq
-	FROM dual;
+   SELECT SEQ_cart_cseq.nextval
+   INTO :new.cseq
+   FROM dual;
 END;
 
-/
+
 
 CREATE OR REPLACE TRIGGER TRI_event_eseq BEFORE INSERT ON event
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_event_eseq.nextval
-	INTO :new.eseq
-	FROM dual;
+   SELECT SEQ_event_eseq.nextval
+   INTO :new.eseq
+   FROM dual;
 END;
 
-/
+
 
 CREATE OR REPLACE TRIGGER TRI_Member_membernum BEFORE INSERT ON Member
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_Member_membernum.nextval
-	INTO :new.membernum
-	FROM dual;
+   SELECT SEQ_Member_membernum.nextval
+   INTO :new.membernum
+   FROM dual;
 END;
 
-/
+
 
 CREATE OR REPLACE TRIGGER TRI_member_mseq BEFORE INSERT ON member
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_member_mseq.nextval
-	INTO :new.mseq
-	FROM dual;
+   SELECT SEQ_member_mseq.nextval
+   INTO :new.mseq
+   FROM dual;
 END;
 
-/
+
 
 CREATE OR REPLACE TRIGGER TRI_order_detail_odseq BEFORE INSERT ON order_detail
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_order_detail_odseq.nextval
-	INTO :new.odseq
-	FROM dual;
+   SELECT SEQ_order_detail_odseq.nextval
+   INTO :new.odseq
+   FROM dual;
 END;
 
-/
+
 
 CREATE OR REPLACE TRIGGER TRI_product_productnum BEFORE INSERT ON product
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_product_productnum.nextval
-	INTO :new.productnum
-	FROM dual;
+   SELECT SEQ_product_productnum.nextval
+   INTO :new.productnum
+   FROM dual;
 END;
 
-/
+
 
 CREATE OR REPLACE TRIGGER TRI_product_pseq BEFORE INSERT ON product
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_product_pseq.nextval
-	INTO :new.pseq
-	FROM dual;
+   SELECT SEQ_product_pseq.nextval
+   INTO :new.pseq
+   FROM dual;
 END;
 
-/
 
 CREATE OR REPLACE TRIGGER TRI_qna_qseq BEFORE INSERT ON qna
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_qna_qseq.nextval
-	INTO :new.qseq
-	FROM dual;
+   SELECT SEQ_qna_qseq.nextval
+   INTO :new.qseq
+   FROM dual;
 END;
 
-/
+
 
 CREATE OR REPLACE TRIGGER TRI_sub_product_spseq BEFORE INSERT ON sub_product
 FOR EACH ROW
 BEGIN
-	SELECT SEQ_sub_product_spseq.nextval
-	INTO :new.spseq
-	FROM dual;
+   SELECT SEQ_sub_product_spseq.nextval
+   INTO :new.spseq
+   FROM dual;
 END;
 
-/
+
 
 create table shortproduct(
-	spseq number(10),
-	pname varchar2(100),
-	kind1 varchar2(5),
-	kind2 varchar2(3),
-	image varchar2(50),
-	useyn varchar2(1)
+   spseq number(10),
+   pname varchar2(100),
+   kind1 varchar2(5),
+   kind2 varchar2(3),
+   image varchar2(50),
+   useyn varchar2(1)
 );
 
 
-insert into product(pseq, pname, price1, price2, price3, kind1, kind2, image, content, useyn)
-values(SEQ_product_pseq.nextVal, '더블화이트갈릭와퍼라지세트', 15000, 0, 0, '2', '1', '', '더블화이트갈릭와퍼+프렌치프라이L+콜라L', 1);
-insert into product(pseq, pname, price1, price2, price3, kind1, kind2, image, content, useyn)
-values(SEQ_product_pseq.nextVal, '더블화이트갈릭와퍼세트', 12000, 0, 0, '2', '2', '', '더블화이트갈릭와퍼+프렌치프라이R+콜라R', 1);
-insert into product(pseq, pname, price1, price2, price3, kind1, kind2, image, content, useyn)
-values(SEQ_product_pseq.nextVal, '더블화이트갈릭와퍼', 8000, 0, 0, '2', '3', 'doublewhitegarlic.png', '단품', 1);
-
-select * from product;
--- WEB_Burgerking/src/main/webapp/Event/eventList.jsp
+++ WEB_Burgerking/src/main/webapp/Event/eventList.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ include file="../header.jsp"%>
+<link href="css/event.css" rel="stylesheet">
 
 <article>
 	<div class="location">
@@ -9,6 +10,30 @@
 			<li>전체</li>
 		</ul>
 	</div>
+<div class="contentbox">
+<div class="eventweb_container">	
+
+<div class="subtit">
+<h1 class="event_tit">이벤트</h1>
+<ul class="submenu_right"> 
+<li><span><a href="burger.do?command=tab1">전체</a></span></li>
+<li><span><a href="burger.do?command=tab2">진행중</a></span></li>
+<li><span><a href="burger.do?command=tab3">종료</a></span></li>
+</ul>
+</div>	
+<div class="eventview">
+<ul class="event_box">
+
+</ul>
+
+
+
+
+</div>
+
+</div>
+</div>
+<	
 </article>
 
 <%@ include file="/undermenu.jsp" %>
