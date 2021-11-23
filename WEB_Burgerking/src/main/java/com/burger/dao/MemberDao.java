c2a49405267e6a1580f07db8e4d8c440ba58b0f7
YataNox <rladnwls0523@naver.com>
2021-11-23 19:10:28 +0900

    Write getMember Method

--- WEB_Burgerking/src/main/java/com/burger/dao/MemberDao.java
+++ WEB_Burgerking/src/main/java/com/burger/dao/MemberDao.java
@@ -3,6 +3,10 @@ package com.burger.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.burger.dto.MemberVO;
+import com.burger.util.DBman;
 
 public class MemberDao {
 	private MemberDao() { }
@@ -12,4 +16,34 @@ public class MemberDao {
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+	
+	public MemberVO getMember(String id) {
+		MemberVO mvo = null;
+		String sql = "select * from member where id = ?";
+		con = DBman.getConnection();
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				mvo = new MemberVO();
+				mvo.setMseq(rs.getInt("mseq"));
+				mvo.setId(rs.getString("id"));
+				mvo.setPwd(rs.getString("pwd"));
+				mvo.setName(rs.getString("name"));
+				mvo.setPhone(rs.getString("phone"));
+				mvo.setIndate(rs.getTimestamp("indate"));
+				mvo.setLastdate(rs.getTimestamp("lastdate"));
+				mvo.setMemberkind(rs.getInt("memberkind"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBman.close(con, pstmt, rs);
+		}
+		
+		return mvo;
+	}
 }
