481dca9e720776da2d455445903d52ebe0050ef9
YataNox <rladnwls0523@naver.com>
2021-11-23 19:08:17 +0900

    Create & Write LoginAction, LogoutAction

---
+++ WEB_Burgerking/src/main/java/com/burger/controller/action/LoginAction.java
@@ -0,0 +1,48 @@
+package com.burger.controller.action;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.burger.dao.MemberDao;
+import com.burger.dto.MemberVO;
+
+public class LoginAction implements Action {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = "member/login.jsp";
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		
+		MemberDao mdao = MemberDao.getInstance();
+		MemberVO mvo = mdao.getMember(id);
+		
+		if(mvo == null) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			writer.println("<script>alert('입력하신 아이디가 없습니다.'); location.href='burger.do?command=loginForm';</script>");
+			writer.close();
+		}else if(mvo.getPwd() == null) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			writer.println("<script>alert('회원정보 오류 관리자에게 문의하세요'); location.href='burger.do?command=loginForm';</script>");
+			writer.close();
+		}else if(!mvo.getPwd().equals(pwd)) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			writer.println("<script>alert('비밀번호가 틀렸습니다.'); location.href='burger.do?command=loginForm';</script>");
+			writer.close();
+		}else {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", mvo);
+			url="burger.do?command=deliveryForm";
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
+	}
+}
