5fd9f8a4541f857a853bb5994377b432fc112fb8
YataNox <rladnwls0523@naver.com>
2021-11-23 21:57:00 +0900

    Add findId/Pwd link

--- WEB_Burgerking/src/main/webapp/script/burger.js
+++ WEB_Burgerking/src/main/webapp/script/burger.js
@@ -10,3 +10,14 @@ function login_chk(){
 	}
 	return true;
 }
+
+function find_id(){
+	var url = "burger.do?command=findIdForm";
+	var opt = "toolbar=no, menubar=no, resizable=no, width=700, height=500, top=300, left=300";
+	window.open(url, "Find Id", opt);
+}
+function find_pwd(){
+	var url = "burger.do?command=findPwdForm";
+	var opt = "toolbar=no, menubar=no, resizable=no, width=700, height=500, top=300, left=300";
+	window.open(url, "Find Pwd", opt);
+}
