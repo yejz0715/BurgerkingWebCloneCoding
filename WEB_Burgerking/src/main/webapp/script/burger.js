34e6c235b66e5dda773add2702b4939d1dd1cb5e
Java02 <Java02@DESKTOP-RDLU2MK>
2021-11-24 21:15:11 +0900

    Write check_input4,5, updatepwdChk function

--- WEB_Burgerking/src/main/webapp/script/burger.js
+++ WEB_Burgerking/src/main/webapp/script/burger.js
@@ -40,6 +40,24 @@ function check_input3(){
 		b.innerText = "아이디를 입력해 주세요.";
 	}
 }
+function check_input4(){
+	var a = document.getElementById("pwd");
+	var b = document.getElementById("pwd_coment");
+	if(a.value != ""){
+		b.innerText = "";
+	}else{
+		b.innerText = "새 비밀번호를 입력해 주세요.";
+	}
+}
+function check_input5(){
+	var a = document.getElementById("pwd_chk");
+	var b = document.getElementById("pwd_chk_coment");
+	if(a.value != ""){
+		b.innerText = "";
+	}else{
+		b.innerText = "비밀번호 재확인";
+	}
+}
 
 function inputidChk(){
 	if(document.frm.name.value==""){
@@ -61,6 +79,18 @@ function inputpwdChk(){
 	}else if(document.frm.id.value==""){
 		alert("아이디를 입력해주세요.");
 		document.frm.id.focus();
+		return false;
+	}
+	return true;
+}
+function updatepwdChk(){
+	if(document.frm.pwd.value==""){
+		alert("비밀번호를 입력해주세요.");
+		document.frm.pwd.focus();
+		return false;
+	}else if(document.frm.pwd.value!=document.frm.pwd_chk.value){
+		alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+		document.frm.pwd_chk.focus();
 		return false;
 	}
 	return true;
