20df000f528ce94f1b481187b998d155ba31bccf
Java02 <Java02@DESKTOP-RDLU2MK>
2021-11-24 20:07:57 +0900

    Add chk parameter line

--- WEB_Burgerking/src/main/java/com/burger/controller/action/FindPwdFormAction.java
+++ WEB_Burgerking/src/main/java/com/burger/controller/action/FindPwdFormAction.java
@@ -9,6 +9,13 @@ import javax.servlet.http.HttpServletResponse;
 public class FindPwdFormAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = "Member/findPwdForm.jsp";
+		if(request.getParameter("id")!="");
+		{
+			String id = request.getParameter("id");
+			String name = request.getParameter("name");
+			request.setAttribute("id", id);
+			request.setAttribute("name", name);
+		}
 		request.getRequestDispatcher(url).forward(request, response);
 	}
 }
