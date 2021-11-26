500f19cacfe95966ac86e0e0ef9052ad4fd6b8f2
JAVA02 <JAVA02@DESKTOP-RIF7972>
2021-11-26 17:01:17 +0900

    changdae image error

--- WEB_Burgerking/src/main/java/com/burger/controller/action/DeliveryDetailFormAction.java
+++ WEB_Burgerking/src/main/java/com/burger/controller/action/DeliveryDetailFormAction.java
@@ -14,7 +14,7 @@ public class DeliveryDetailFormAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url="delivery/deliveryDetail.jsp";
+		String url="Delivery/deliveryDetail.jsp";
 		
 		ProductDao pdao = ProductDao.getInstance();
 		
